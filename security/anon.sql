d2bed6077012ebfbbb67899dd4d55da2f4d34428
steve-chavez <stevechavezast@gmail.com>
2019-04-23 10:34:52 -0500

    Refactor policies into dirs

---
+++ security/anon.sql
@@ -0,0 +1,53 @@
+-----------------
+--users_details--
+-----------------
+
+grant
+  select
+on users_details
+to socnet_anon;
+
+create policy anon_select_policy
+on users_details
+for select
+to socnet_anon
+using (
+  users_details.audience = 'public'
+);
+
+---------
+--posts--
+---------
+
+grant
+  select
+on posts
+to socnet_anon;
+
+create policy anon_select_policy
+on posts
+for select
+to socnet_anon
+using (
+  posts.audience = 'public'
+);
+
+------------
+--comments--
+------------
+
+grant
+  select
+on comments
+to socnet_anon;
+
+create policy anon_select_policy
+on comments
+for select
+to socnet_anon
+using (
+  exists (
+    select 1
+    from posts
+    where posts.id = comments.post_id)
+);
